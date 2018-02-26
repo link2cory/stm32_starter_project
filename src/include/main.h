b812bf43d3b64c04d8939f8270abbf9c19ce8497
Cory <link2cory@gmail.com>
2018-02-26 08:47:47 -0800

    added basic application source code and startup script

---
+++ src/include/main.h
@@ -0,0 +1,9 @@
+/*******************************************************************************
+* Application: Blinky
+* Filename:    main.h
+* Author:      Cory Perkins
+*******************************************************************************/
+
+#include "stm32f4xx.h"
+#include "stm32f4xx_nucleo.h"
+#include "stm32f4xx_hal.h"
